f2d364485bf06c05084c98470b74e1317c000592
JoeFromJax <jlucas6124@gmail.com>
2023-01-03 15:01:22 -0500

    Homework update modules being funky

--- index.js
+++ index.js
@@ -1,3 +1,55 @@
-import axios from "axios";
-// we use `import axios from "axios"` which is another way of saying `const axios = require("axios")`
-// it is jsut better supported in the browser!
+import { Alchemy, Network } from "alchemy-sdk";
+
+const settings = {
+  apiKey: "D5jtQTYVyu9_tx2jInmHBWIcmoUogGch",
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+
+async function main() {
+  const walletAddress = document.getElementById('wallet-address').value;
+  const nfts = await alchemy.nft.getNftsForOwner(walletAddress);
+  const nftDiv = document.getElementById('nfts');
+  nftDiv.innerHTML = ''; // Clear existing NFTs
+  for (const nft of nfts.ownedNfts) {
+    // Create an img element for the NFT
+    const img = document.createElement('img');
+    img.src = nft.thumbnail;
+    img.alt = nft.title;
+    // Append the img element to the NFT div
+    nftDiv.appendChild(img);
+  }
+
+  // Get token balances
+  const balances = await alchemy.core.getTokenBalances(walletAddress);
+  // Remove tokens with zero balance
+  const nonZeroBalances = balances.tokenBalances.filter((token) => {
+    return token.tokenBalance !== '0';
+  });
+  console.log(`Token balances of ${walletAddress}:`);
+  // Counter for SNo of final output
+  let i = 1;
+
+  // Loop through all tokens with non-zero balance
+  for (const token of nonZeroBalances) {
+    // Get balance of token
+    let balance = token.tokenBalance;
+    // Get metadata of token
+    const metadata = await alchemy.core.getTokenMetadata(token.contractAddress);
+
+    // Compute token balance in human-readable format
+    balance = balance / Math.pow(10, metadata.decimals);
+    balance = balance.toFixed(2);
+
+    // Print name, balance, and symbol of token
+    console.log(`${i++}. ${metadata.name}: ${balance} ${metadata.symbol}`);
+  }
+}
+
+async function fetchNFTs() {
+  await main();
+}
+
+const button = document.getElementById('submit-button');
+button.addEventListener('click', fetchNFTs);
